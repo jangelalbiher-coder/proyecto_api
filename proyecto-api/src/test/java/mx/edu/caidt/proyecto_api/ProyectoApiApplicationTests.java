0168de23b56f687a30fe21d88743a82219b855b9
Angel Albino <jangelherr2210@gmail.com>
2026-03-15 19:52:42 -0600

    Initial commit

---
+++ proyecto-api/src/test/java/mx/edu/caidt/proyecto_api/ProyectoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.caidt.proyecto_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
